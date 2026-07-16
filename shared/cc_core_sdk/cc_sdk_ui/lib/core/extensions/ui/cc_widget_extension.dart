7a8dd94da1d5a79b16e4937d9d07eeccdf48b5c6
Tran Huy <huytower1990@gmail.com>
2026-07-16 21:18:52 +0700

    fix: restore submodule boundary on qltc branch

--- shared/cc_core_sdk/cc_sdk_ui/lib/core/extensions/ui/cc_widget_extension.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension CcWidgetExtension on Widget {
-  Widget visibleWhen(bool condition()) {
-    return Visibility(visible: condition(), child: this);
-  }
-}
