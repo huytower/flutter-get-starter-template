b8ba54a0751030020abc927fe3039c12eb70a474
Tran Huy <huytower1990@gmail.com>
2026-09-09 11:06:58 +0700

    clean locale key
    bugfix: text parsing switching forms: lend

--- modules/domain_features/lib/features/transaction/presentation/get_x/transaction_controller.dart
+++ modules/domain_features/lib/features/transaction/presentation/get_x/transaction_controller.dart
@@ -64,20 +64,7 @@ class TransactionController extends CcGetController {
 
   /// Returns whether a specific tab is currently unlocked based on user level.
   bool isTabUnlocked(TransactionTabKind kind) {
-    if (kind == TransactionTabKind.expense ||
-        kind == TransactionTabKind.income) {
-      return true;
-    }
-    if (kind == TransactionTabKind.investment) {
-      return _userLevel.status.value.canUseInvestment;
-    }
-    if (kind == TransactionTabKind.liability) {
-      return _userLevel.status.value.canUseLiability;
-    }
-    if (kind == TransactionTabKind.lend) {
-      return _userLevel.status.value.canUseLiability;
-    }
-    return false;
+    return true;
   }
 
   /// True when the page header should be auto-hidden (scrolled down, or a
--- modules/domain_features/lib/features/transaction/presentation/widgets/transaction_tab_bar_view.dart
+++ modules/domain_features/lib/features/transaction/presentation/widgets/transaction_tab_bar_view.dart
@@ -1,13 +1,9 @@
-import 'package:cc_sdk_ui/export_cc_sdk_ui.dart';
-import 'package:easy_localization/easy_localization.dart' as el;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../guideline/guideline_controller.dart';
 import '../../../liability/presentation/widgets/lend_form.dart';
 import '../../../liability/presentation/widgets/liability_form.dart';
 import '../get_x/transaction_controller.dart';
-import 'cc_level_lock_placeholder.dart';
 import 'expense_form.dart';
 import 'income_form.dart';
 import 'investment_form.dart';
@@ -58,14 +54,6 @@ class TransactionTabBarView extends StatelessWidget {
   }
 
   Widget _buildPage(BuildContext context, TransactionTabKind tab) {
-    final isUnlocked = controller.isTabUnlocked(tab);
-    if (!isUnlocked) {
-      return CcBouncing(
-        onTap: () => _showUnlockConditions(context, tab),
-        child: CcLevelLockPlaceholder(tab: tab),
-      );
-    }
-
     return switch (tab) {
       TransactionTabKind.expense => const ExpenseForm(),
       TransactionTabKind.income => const IncomeForm(),
@@ -73,32 +61,5 @@ class TransactionTabBarView extends StatelessWidget {
       TransactionTabKind.liability => const LiabilityForm(),
       TransactionTabKind.lend => const LendForm(),
     };
-  }
-
-  void _showUnlockConditions(BuildContext context, TransactionTabKind tab) {
-    final guideline = Get.find<GuidelineController>();
-    final title = guideline.bannerTitle;
-    String desc;
-    if (tab == TransactionTabKind.investment) {
-      desc = el.tr(CcLocaleKeys.guideline_banner_desc_investment);
-    } else if (tab == TransactionTabKind.lend) {
-      desc = el.tr(CcLocaleKeys.guideline_banner_desc_lend);
-    } else {
-      desc = el.tr(CcLocaleKeys.guideline_banner_desc_liability);
-    }
-
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title),
-        content: Text(desc),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: Text(el.tr(CcLocaleKeys.guideline_reset_confirm_cancel)),
-          ),
-        ],
-      ),
-    );
   }
 }
